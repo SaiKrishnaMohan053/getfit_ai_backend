9867ce200feecf9bf28d2cd970b0b380dc9941e3
saikrishnamohan053 <saikrishnamohan.k@gmail.com>
2025-12-08 13:33:34 -0600

    update ECS task definition for backend deployment

--- src/server.js
+++ src/server.js
@@ -12,6 +12,6 @@ const app = require("./app");
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   logger.info(`Server listening on port ${PORT}`);
 });
